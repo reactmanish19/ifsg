9fb9bac7847a683cfddc71214c2addf80e25d6a7
Manish <reactmanish19@gmail.com>
2022-06-23 19:52:45 +0530

    Creates Checkbox for selecting Options in step 2

--- src/Components/MultiStepForm/StepOne/StepOne.js
+++ src/Components/MultiStepForm/StepOne/StepOne.js
@@ -1,51 +1,83 @@
-import React from 'react'
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import "./StepOne.scss"
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
 
+import IconButton from "@mui/material/IconButton";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
+import "./StepOne.scss";
 
 const StepOne = () => {
-    return (
-        <>
-            <div className='step-header'>
-                <div className='step-header__left-section'>
-                    <IconButton aria-label="Back Arrow" color="success">
-                        <ArrowBackIosIcon />
-                    </IconButton>
-                </div>
-                <div className='step-header__middle-section'>
-                    <div className='step-heading'>Digital Assets Onboarding</div>
-                    <div className='step-indicator'>Step 5/8</div>
-                </div>
-                <div className='step-header__right-section'>
-                    <IconButton aria-label="Back Arrow" color="success">
-                        <ArrowForwardIosIcon />
-                    </IconButton>
-                </div>
-            </div>
+  const [checked, setChecked] = useState([]);
+  const checkboxArray = [
+    "EMPLOYMENT INCOME",
+    "SAVINGS / DEPOSITS",
+    "SAVINGS / DEPOSITS",
+    "SALE OF SHARES OR OTHER INVESTMENT",
+    "LOAN",
+    "COMPANY SALE",
+    "COMPANY PROFITS / DIVIDENDS",
+    "INHERITANCE",
+  ];
+  const handleCheck = (e) => {
+    var updatedList = [...checked];
+    if (e.target.checked) {
+      updatedList = [...checked, e.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(e.target.value), 1);
+    }
+    setChecked(updatedList);
+  }
+  console.log(checked,"CHECKBOX ARRAY")
+  return (
+    <>
+      <div className="step-header">
+        <div className="step-header__left-section">
+          <IconButton aria-label="Back Arrow" color="success">
+            <ArrowBackIosIcon />
+          </IconButton>
+        </div>
+        <div className="step-header__middle-section">
+          <div className="step-heading">Digital Assets Onboarding</div>
+          <div className="step-indicator">Step 5/8</div>
+        </div>
+        <div className="step-header__right-section">
+          <IconButton aria-label="Back Arrow" color="success">
+            <ArrowForwardIosIcon />
+          </IconButton>
+        </div>
+      </div>
 
-            <div className='step-question-container'>
-                <div className='step-question'>How were the assets generated that are being initially transferred?</div>
-                <div className='step-question-instruction'>(Select all that apply)</div>
+      <div className="step-question-container">
+        <div className="step-question">
+          How were the assets generated that are being initially transferred?
+        </div>
+        <div className="step-question-instruction">(Select all that apply)</div>
 
-                <div className='step-question-option-container'>
-                    <div className='step-question-option'>EMPLOYMENT INCOME</div>
-                    <div className='step-question-option'>SAVINGS / DEPOSITS</div>
-                    <div className='step-question-option'>SALE OF SHARES OR OTHER INVESTMENT</div>
-                    <div className='step-question-option'>LOAN</div>
-                    <div className='step-question-option'>COMPANY SALE</div>
-                    <div className='step-question-option'>COMPANY PROFITS / DIVIDENDS</div>
-                    <div className='step-question-option'>INHERITANCE</div>
-                </div>
-            </div>
+        <div className="step-question-option-container">
+          <ul class="ks-cboxtags">
+            {checkboxArray.map((check, ind) => {
+              return (
+                <li>
+                  <input
+                    type="checkbox"
+                    id={ind}
+                    value={check}
+                    onChange={handleCheck}
+                  />
+                  <label for="checkboxOne">{check}</label>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
 
-            <div className='step-footer'>
-                <Button variant="contained">NEXT</Button>
-            </div>
-        </>
-    )
-}
-export default StepOne
+      <div className="step-footer">
+        <Button variant="contained">NEXT</Button>
+      </div>
+    </>
+  );
+};
+export default StepOne;
--- src/Components/MultiStepForm/StepSecond/StepSecond.js
+++ src/Components/MultiStepForm/StepSecond/StepSecond.js
@@ -1,84 +1,117 @@
-import { FormControl, InputLabel, MenuItem, Select, TextField,Checkbox,Button } from '@mui/material'
-import React, { useState } from 'react'
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Checkbox,
+  Button,
+} from "@mui/material";
+import InputAdornment from "@mui/material/InputAdornment";
+import React, { useState } from "react";
 const StepSecond = () => {
-    const [values, setValues] = useState('')
-    // const handleChange=(e)=>{
-    //     e.preventDefault()
-    //     alert('hhhh')
-    //     setValues('kkk')
+  const [values, setValues] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("SUBMIT");
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <FormControl fullWidth>
+          <InputLabel id="Asset_name">Asset name</InputLabel>
+          <Select
+            variant="outlined"
+            labelId="Asset_name"
+            label="Asset name"
+            name="Asset_name"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+            value={values.Asset_name}
+          >
+            <MenuItem value={"BitCoin"}>BitCoin</MenuItem>
+            <MenuItem value={"Etherium"}>Etherium</MenuItem>
+            <MenuItem value={"Doge"}>Doge</MenuItem>
+          </Select>
+        </FormControl>
 
-    // }
-    console.log(values)
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        alert('SUBMIT')
-    }
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-            <FormControl fullWidth >
-                {/* <label>Specify your digital asset name</label> */}
-                <InputLabel id='Asset_name'>Asset name</InputLabel>
-                <Select
-                    // focused
-                    variant="outlined" 
-                    labelId="Asset_name"
-                    // value={}
-                    label="Asset name"
-                    name='Asset_name'
-                    onChange={(e) => setValues({ [e.target.name]: e.target.value })}
-                    value={values.Asset_name}
-                >
-                    <MenuItem value={'BitCoin'}>BitCoin</MenuItem>
-                    <MenuItem value={'Etherium'}>Etherium</MenuItem>
-                    <MenuItem value={'Doge'}>Doge</MenuItem>
-                </Select>
-                {/* <InputLabel>Specify the costudion name</InputLabel> */}
-                <TextField label="Custodion Name" name='Custodion_Name' value={values.Asset_name} onChange={(e) => setValues({ [e.target.name]: e.target.value })} color="secondary"  />
+        <TextField
+          label="Custodion Name"
+          name="Custodion_Name"
+          value={values.Custodion_Name}
+          onChange={(e) =>
+            setValues({ ...values, [e.target.name]: e.target.value })
+          }
+          color="secondary"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                {values.Asset_name}
+              </InputAdornment>
+            ),
+          }}
+        />
 
-                {/* <InputLabel>Specify the wallet address</InputLabel> */}
+        <TextField
+          label="Wallet Address"
+          name="Wallet_Address"
+          value={values.Wallet_Address}
+          onChange={(e) =>
+            setValues({ ...values, [e.target.name]: e.target.value })
+          }
+          color="secondary"
+        />
+        <FormControl fullWidth>
+          <InputLabel id="Select">Select</InputLabel>
+          <Select
+            variant="outlined"
+            labelId="Select"
+            label="Select"
+            name="Select"
+            onChange={(e) =>
+              setValues({ ...values, [e.target.name]: e.target.value })
+            }
+            value={values.Select}
+          >
+            <MenuItem value={"SelectOne"}>SelectOne</MenuItem>
+            <MenuItem value={"SelectTwo"}>SelectTwo</MenuItem>
+            <MenuItem value={"SelectThree"}>SelectThree</MenuItem>
+          </Select>
+        </FormControl>
 
-                <TextField label="Wallet Address" name='Wallet_Address' value={values.Wallet_Address} onChange={(e) => setValues({ [e.target.name]: e.target.value })} color="secondary"  />
-                <Select
-                    // focused
-                    variant="outlined"
-                    // value={}
-                    labelId="Select"
-                    label="Select"
-                    name='Select'
-                    onChange={(e) => setValues({ [e.target.name]: e.target.value })}
-                    value={values.Select}
-                >
-                    <MenuItem value={'SelectOne'}>SelectOne</MenuItem>
-                    <MenuItem value={'SelectTwo'}>SelectTwo</MenuItem>
-                    <MenuItem value={'SelectThree'}>SelectThree</MenuItem>
-                {/* <InputLabel id='Select'>Select</InputLabel> */}
-                </Select>
-                <TextField label="Custodion Name" name='Custodion_Name_Two' value={values.Custodion_Name_Two} oonChange={(e) => setValues({ [e.target.name]: e.target.value })} color="secondary" />
-                <TextField label="Wallet Address" name='Wallet_Address_Two' value={values.Wallet_Address_Two} onChange={(e) => setValues({ [e.target.name]: e.target.value })} color="secondary" />
-                
-               <Checkbox name='checkbox' onChange={(e) => setValues({ [e.target.name]: e.target.checked })} checked={values.checkbox?true:false} /> 
-               
-               <Button variant="contained" type='submit'>Next</Button>
-            </FormControl>
+        <TextField
+          label="Custodion Name"
+          name="Custodion_Name_Two"
+          value={values.Custodion_Name_Two}
+          onChange={(e) =>
+            setValues({ ...values, [e.target.name]: e.target.value })
+          }
+          color="secondary"
+        />
+        <TextField
+          label="Wallet Address"
+          name="Wallet_Address_Two"
+          value={values.Wallet_Address_Two}
+          onChange={(e) =>
+            setValues({ ...values, [e.target.name]: e.target.value })
+          }
+          color="secondary"
+        />
 
-            </form>
+        <Checkbox
+          name="checkbox"
+          onChange={(e) =>
+            setValues({ ...values, [e.target.name]: e.target.checked })
+          }
+          checked={values.checkbox ? true : false}
+        />
 
-
-
-
-
-
-
-
-
-
-
-            {/* <label>Specify your digital asset name</label>
-            <br /> */}
-            {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" size="small" color="warning" /> */}
-
-        </>
-    )
-}
-export default StepSecond
+        <Button variant="contained" type="submit">
+          Next
+        </Button>
+      </form>
+    </>
+  );
+};
+export default StepSecond;
