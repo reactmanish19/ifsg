4887da8fba589f390b8ff7e8dfa2fba172280f00
Manish <reactmanish19@gmail.com>
2022-06-24 15:32:09 +0530

    Add Dark Theme Support

--- src/Components/MultiStepForm/StepSecond/StepSecond.js
+++ src/Components/MultiStepForm/StepSecond/StepSecond.js
@@ -1,17 +1,13 @@
-import {
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-  Checkbox,
-  Button,
-  IconButton,
-} from "@mui/material";
-import InputAdornment from "@mui/material/InputAdornment";
 import React, { useState } from "react";
+import { FormControl, InputLabel, MenuItem, Select, TextField, Checkbox, Button, IconButton, FormGroup, FormControlLabel, } from "@mui/material";
+import InputAdornment from "@mui/material/InputAdornment";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import ArrowForwardIos from "@mui/icons-material/ArrowForwardIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+import "./StepSecond.scss"
+
 const StepSecond = () => {
   const [values, setValues] = useState({});
   const [createFields, setCreateFields] = useState([0]);
@@ -20,94 +16,136 @@ const StepSecond = () => {
     e.preventDefault();
     alert("SUBMIT");
   };
+
   const AddFields = (e, ind) => {
     e.preventDefault()
     var updatedList = [...createFields];
-    if(ind===0){
-        updatedList.push(updatedList.length)
-        setCreateFields(updatedList)
+    if (ind === 0) {
+      updatedList.push(updatedList.length)
+      setCreateFields(updatedList)
     }
-    else{
-        const remove= updatedList.filter((v,i)=> i!==ind )
-        setCreateFields(remove)
+    else {
+      const remove = updatedList.filter((v, i) => i !== ind)
+      setCreateFields(remove)
     }
   };
+
+  const label = { inputProps: { 'aria-label': `I don't hold any digital currency` } };
+
+
   return (
     <>
+
+      <div className="step-header">
+        <div className="step-header__left-section">
+          <IconButton aria-label="Back Arrow" color="success">
+            <ArrowBackIosIcon />
+          </IconButton>
+        </div>
+        <div className="step-header__middle-section">
+          <div className="step-heading">Digital Assets Onboarding</div>
+          <div className="step-indicator">Step 6/8</div>
+        </div>
+        <div className="step-header__right-section">
+          <IconButton aria-label="Back Arrow" color="success">
+            <ArrowForwardIosIcon />
+          </IconButton>
+        </div>
+      </div>
+
       <form onSubmit={handleSubmit}>
-        {createFields.map((val, index) => {
-          return (
-            <>
-              <FormControl fullWidth>
-                <InputLabel id="Asset_name">Asset name</InputLabel>
-                <Select
-                  variant="outlined"
-                  labelId="Asset_name"
-                  label="Asset name"
-                  name={`Asset_name${val}`}
-                  onChange={(e) =>
-                    setValues({ ...values, [e.target.name]: e.target.value })
-                  }
-                  value={values[`Asset_name${val}`]}
-                >
-                  <MenuItem value={"BitCoin"}>BitCoin</MenuItem>
-                  <MenuItem value={"Etherium"}>Etherium</MenuItem>
-                  <MenuItem value={"Doge"}>Doge</MenuItem>
-                </Select>
-              </FormControl>
+        <div className="step-question-container">
+          <div className="step-question-option-container">
+            {createFields.map((val, index) => {
+              return (
+                <>
 
-              <TextField
-                label="Custodion Name"
-                name={`Custodion_Name${val}`}
-                value={values[`Custodion_Name${val}`]}
-                onChange={(e) =>
-                  setValues({ ...values, [e.target.name]: e.target.value })
-                }
-                color="secondary"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="start">
-                        {/* `Asset_name${val}` */}
-                      {values[`Asset_name${val}`]}
-                    </InputAdornment>
-                  ),
-                }}
-              />
+                  <div className="step-question-form-container">
+                    <FormControl>
+                      <InputLabel id="Asset_name" color="secondary">Select</InputLabel>
+                      <Select
+                        variant="outlined"
+                        labelId="Asset_name"
+                        label="Select"
+                        name={`Asset_name${val}`}
+                        onChange={(e) =>
+                          setValues({ ...values, [e.target.name]: e.target.value })
+                        }
+                        value={values[`Asset_name${val}`]}
+                        className="digital-asset-name"
+                        color="secondary"
+                      >
+                        <MenuItem value={"BitCoin"}>BitCoin</MenuItem>
+                        <MenuItem value={"Etherium"}>Etherium</MenuItem>
+                        <MenuItem value={"Doge"}>Doge</MenuItem>
+                      </Select>
+                    </FormControl>
 
-              <TextField
-                label="Wallet Address"
-                name={`Wallet_Address${val}`}
-                value={values[`Wallet_Address${val}`]}
-                onChange={(e) =>
-                  setValues({ ...values, [e.target.name]: e.target.value })
-                }
-                color="secondary"
-              />
-              <IconButton
-                aria-label="Minus"
-                color="success"
-                onClick={(e)=>AddFields(e,index)}
-              >
-                {
-                    val===0? <ArrowForwardIos /> : <ArrowBackIosIcon />
-                }
-              </IconButton>
-            </>
-          );
-        })}
+                    <TextField
+                      label="Custodian name"
+                      name={`Custodion_Name${val}`}
+                      value={values[`Custodion_Name${val}`]}
+                      onChange={(e) =>
+                        setValues({ ...values, [e.target.name]: e.target.value })
+                      }
+                      color="secondary"
+                      className="custodian-name"
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="start">
+                            {/* `Asset_name${val}` */}
+                            {values[`Asset_name${val}`]}
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
 
-        <Checkbox
-          name="checkbox"
-          onChange={(e) =>
-            setValues({ ...values, [e.target.name]: e.target.checked })
-          }
-          checked={values.checkbox ? true : false}
-        />
+                    <TextField
+                      label="Wallet address"
+                      name={`Wallet_Address${val}`}
+                      value={values[`Wallet_Address${val}`]}
+                      onChange={(e) =>
+                        setValues({ ...values, [e.target.name]: e.target.value })
+                      }
+                      color="secondary"
+                      className="wallet-address"
+                    />
 
-        <Button variant="contained" type="submit">
-          Next
-        </Button>
+                    <IconButton
+                      aria-label="Minus"
+                      color="success"
+                      onClick={(e) => AddFields(e, index)}
+                      className="add-remove-button"
+                    >
+                      {val === 0 ? <AddIcon /> : <RemoveIcon />}
+                    </IconButton>
+                  </div>
+                </>
+              );
+            })}
+            <div className="i-do-not-hold-any-digital-currency">
+              <FormGroup>
+                <FormControlLabel
+                  control={<Checkbox
+                    name="checkbox"
+                    onChange={(e) =>
+                      setValues({ ...values, [e.target.name]: e.target.checked })
+                    }
+                    checked={values.checkbox ? true : false}
+                  />}
+                  label="I don't hold any digital currency"
+                />
+              </FormGroup>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="step-footer">
+          <Button className="footer-navigation-button" size="large" type="submit">NEXT</Button>
+        </div>
       </form>
+
     </>
   );
 };
