875c143b4a47538de4a60a206292a1f5b999ab34
Manish <reactmanish19@gmail.com>
2022-06-29 13:36:46 +0530

    Use React-Query

---
+++ src/Components/MultiStepForm/ReactQueryStep/ReactQueryStep.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+const ReactQueryStep = () => {
+  const [characters, SetCharacters] = useState([]);
+  const FetchCharacter = async () => {
+    const response = await fetch("https://rickandmortyapi.com/api/character"); //https://rickandmortyapi.com/api/character
+   return response.json();
+  };
+  const {isLoading, error,data} =useQuery("characters",FetchCharacter)
+  if(isLoading){
+    return(
+        <div>LOADING...</div>
+    )
+  }
+  if(error){
+    return(
+    <div>Error Occured :  {error.message}</div>
+    )
+  }
+
+  return (
+    <div>
+        {data.results.map((characters) => (
+      
+        <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={characters.image}
+          alt="Character Image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+          {characters.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           status - {characters.status}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           species - {characters.species}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           location - {characters.location.name}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+      ))}
+      
+    </div>
+  );
+};
+export default ReactQueryStep;
--- src/Routes/AllRoutes.js
+++ src/Routes/AllRoutes.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactQueryStep from '../Components/MultiStepForm/ReactQueryStep/ReactQueryStep';
 
 import StepOne from '../Components/MultiStepForm/StepOne/StepOne';
 import StepSecond from '../Components/MultiStepForm/StepSecond/StepSecond';
 import Layout from '../Layout/Layout';
-
+import { QueryClient,QueryClientProvider } from 'react-query';
 const AllRoutes = () => {
+    const queryClient=new QueryClient()
     return (
         <Router>
             <Switch>
                 <Layout>
+                    <QueryClientProvider client={queryClient}>
                     <Route path="/" component={StepOne} exact />
                     <Route path="/stepSecond" component={StepSecond} />
+                    <Route path="/reactquery" component={ReactQueryStep} />
+                    </QueryClientProvider>
                 </Layout>
             </Switch>
         </Router >
